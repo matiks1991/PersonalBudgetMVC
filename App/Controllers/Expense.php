db98445b00d59faaaaab4a7f65b0264a33478ea1
Mateusz Broniszewski <mateusz.broniszewski.programista@gmail.com>
2020-10-03 22:20:43 +0200

    Added save expense - Controlers/Expenses.php

--- App/Controllers/Expense.php
+++ App/Controllers/Expense.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use App\Flash;
 use App\Models\Expenses;
 use Core\View;
 use \App\Time;
@@ -19,26 +20,36 @@ class Expense extends Authenticated
 
     /**
      * Render the expense page
-     *
+     * @param arguments
      * @return void
      */
     public function indexAction($arguments = [])
     {
         $arguments['paymentMethods'] = Expenses::getPaymentMethods();
         $arguments['expensesCategory'] = Expenses::getExpensesCategory();
-        $arguments['currentDate'] = Time::getCurrentDate();
+        $arguments['date'] = Time::getCurrentDate();
         View::renderTemplate('Expense/index.html', $arguments);
     }
 
-    //Add a new expense
-    public function newAction()
+    /**
+     * Add a new expense
+     * @param arguments
+     * @return void
+     */
+    public function newAction($arguments = [])
     {
-        echo "new action";
+        $success = Expenses::saveExpense();
+
+        if ($success)
+            Flash::addMessage('Wydatek dodano pomyślenie!');
+
+        $arguments['paymentMethods'] = Expenses::getPaymentMethods();
+        $arguments['expensesCategory'] = Expenses::getExpensesCategory();
+        $arguments['amount'] = $_POST['amount'];
+        $arguments['date'] = $_POST['date'];
+        $arguments['comment'] = $_POST['comment'];
+
+        View::renderTemplate('Expense/index.html', $arguments);
     }
 
-    //Show an expense
-    public function showAction()
-    {
-        echo "show action";
-    }
 }
