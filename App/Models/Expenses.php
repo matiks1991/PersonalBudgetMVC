056f71aa94c7c02eac383857143b9509c54a39e1
Mateusz Broniszewski <mateusz.broniszewski.programista@gmail.com>
2020-10-03 22:19:10 +0200

    Added save expense - Models/Expenses.php

--- App/Models/Expenses.php
+++ App/Models/Expenses.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use App\Flash;
+use App\Time;
 use PDO;
 
 /**
  * Expenses Model
- * 
+ *
  * PHP 7.0
  */
 
@@ -15,7 +17,7 @@ use PDO;
 
     /**
      * Get payment methods
-     *
+     * @param Session user_id
      * @return array
      */
     public static function getPaymentMethods()
@@ -25,8 +27,6 @@ use PDO;
         $db = static::getDB();
         $stmt = $db->prepare($sql);
 
-        // $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
-
         $stmt->execute();
 
         return $stmt->fetchAll();
@@ -34,7 +34,7 @@ use PDO;
 
     /**
      * Get category expenses
-     *
+     * @param Session user_id
      * @return array
      */
     public static function getExpensesCategory()
@@ -47,5 +47,69 @@ use PDO;
         $stmt->execute();
 
         return $stmt->fetchAll();
+    }
+
+    /**
+     * Save a expense
+     * @param form method post
+     * @return boolean success = true
+     */
+    public static function saveExpense()
+    {
+        if (static::validateExpense()) {
+            $comment = htmlentities($_POST['comment']);
+
+            $sql = "INSERT INTO expenses ( id, user_id, expense_category, 	payment_method, amount, date, comment) VALUES ( 'NULL', :user_id, :category, :paymentMethod, :amount, :date, :comment)";
+
+            $db = static::getDB();
+            $stmt = $db->prepare($sql);
+
+            $stmt->bindValue(':amount', $_POST['amount'], PDO::PARAM_INT);
+            $stmt->bindValue(':date', '2000-02-02', PDO::PARAM_STR);
+            $stmt->bindValue(':paymentMethod', $_POST['paymentMethod'], PDO::PARAM_INT);
+            $stmt->bindValue(':category', $_POST['expenseCategory'], PDO::PARAM_INT);
+            $stmt->bindValue(':comment', $comment, PDO::PARAM_STR);
+            $stmt->bindValue(':user_id', $_SESSION['user_id'], PDO::PARAM_INT);
+            
+            return $stmt->execute();
+        }
+
+        return false;
+    }
+
+    /**
+     * Validate a expense
+     * @param form method post
+     * @return boolean success = true
+     */
+    protected static function validateExpense()
+    {
+        $result = true;
+
+        if ((!isset($_POST['amount'])) || (!isset($_POST['date'])) || (!isset($_POST['paymentMethod'])) || (!isset($_POST['expenseCategory']))) {
+            Flash::addMessage('Wypełnij wszystkie wymagane pola!', Flash::WARNING);
+            $result = false;
+        }
+
+        if(!is_numeric($_POST['amount'])) {
+            Flash::addMessage('Wprowadzona kwota musi być liczbą!', Flash::WARNING);
+            $result = false;
+        }
+
+        if(strlen($_POST['amount']) > 8) {
+            Flash::addMessage('Wprowadzona kwota może mieć maksymalnie 8 znaków!', Flash::WARNING);
+            $result = false;
+        }
+
+        if (!preg_match('/^\d{4}\-\d{2}\-\d{2}$/', $_POST['date']))
+        {
+            Flash::addMessage('Wprowadź prawidłowy format daty! [DD.MM.RRRR]', Flash::WARNING);
+            $result = false;
+        } elseif(Time::checkDate($_POST['date'])) {
+            Flash::addMessage('Wprowadź rzeczywistą datę!', Flash::WARNING);
+            $result = false;
+        }
+
+        return $result;
     }
  }
