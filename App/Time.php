3cee3d2e34986b36e6c6e6fea92df99c6a1c5e72
Mateusz Broniszewski <mateusz.broniszewski.programista@gmail.com>
2020-10-03 22:14:57 +0200

    Added checkdate() in Time.php

--- App/Time.php
+++ App/Time.php
@@ -13,10 +13,23 @@ use DateTime;
 class Time
 {
 
-    //Return curren date
+    /** Return current date
+     * 
+     * @return date (Y-m-d)
+     */
     public static function getCurrentDate()
     {
         $date = new DateTime();
         return $date->format('Y-m-d');
+    }
+
+    /** Validate date
+     * @param date, string format
+     * @return boolean
+     */
+    static function checkDate($date, $format = 'Y-m-d')
+    {
+        $d = DateTime::createFromFormat($format, $date);
+        return $d && $d->format($format) == $date;
     }
 }
